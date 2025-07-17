8915d5abc0ffb40cecce52223422d51c05adfb7b
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-07-17 19:32:57 +0900

    refactor: refactor

--- js/src/run.ts
+++ js/src/run.ts
@@ -118,7 +118,8 @@ export const main = async () => {
   if (action === "client1") {
     // Generate artifact name
     const n = nowS();
-    const artifactName = `securefix-${n}-${Array.from({ length: 50 - `securefix-${n}-`.length }, () => Math.floor(Math.random() * 36).toString(36)).join("")}`;
+    const prefix = `securefix-${n}-`;
+    const artifactName = `${prefix}${Array.from({ length: 50 - prefix.length }, () => Math.floor(Math.random() * 36).toString(36)).join("")}`;
     core.setOutput("artifact_name", artifactName);
     // List fixed files
     const result = await exec.getExecOutput("git", ["ls-files", "--modified", "--others", "--exclude-standard"]);
