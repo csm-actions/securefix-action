72978c58d3c9e7fcb59b86d6d2ccfc378b5569f3
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-11-17 00:15:59 +0900

    fix: use await

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import * as core from "@actions/core";
 import { main } from "./run";
 
 try {
-  main();
+  await main();
 } catch (error) {
   const msg = error instanceof Error ? error.message : JSON.stringify(error);
-  core.debug(`Set output error=${msg}`);
+  core.debug(`output error=${msg}`);
   core.setOutput("error", msg);
   core.setFailed(msg);
 }
